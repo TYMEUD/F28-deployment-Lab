3e6f2385c5757bae6761bd266a9f059e2145e0ce
Ty Menlove <tjmundecided@gmail.com>
2022-09-21 14:43:07 -0600

    added client folder and main.js

--- server/index.js
+++ server/index.js
@@ -10,7 +10,9 @@ const app = express()
 
 app.use(express.json())
 
-app.use('/', express.static(path.join(__dirname, './index.html')))
+app.use('/', express.static(path.join(__dirname, '..client/index.html')))
+
+app.use(express.static(path.join(__dirname, '../client')))
 
 
 app.listen(port, () => {
