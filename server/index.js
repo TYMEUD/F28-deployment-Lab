e4b80649e26fd16c9e49e0f4068660f4ee6c48d5
Ty Menlove <tjmundecided@gmail.com>
2022-09-21 14:31:23 -0600

    added another .use to the index.js

--- server/index.js
+++ server/index.js
@@ -10,6 +10,8 @@ const app = express()
 
 app.use(express.json())
 
+app.use('/', express.static(path.join(__dirname, './index.html')))
+
 app.post('/register', (req, res) => res.status(200)).send('Registered')
 
 app.listen(port, () => {
